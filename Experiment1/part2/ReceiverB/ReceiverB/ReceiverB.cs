9fd17b56cfca8bdb14e38a26d5654e9f0b38517c
WHUCSstetman <105272933+WHUCSstetman@users.noreply.github.com>
2024-02-17 16:17:24 +0800

    Add files via upload

---
+++ Experiment1/part2/ReceiverB/ReceiverB/ReceiverB.cs
@@ -0,0 +1,41 @@
+﻿using System.Runtime.InteropServices;
+using System;
+using System.Windows.Forms;
+using CopyDataStruct;
+
+namespace ReceiverB
+{
+    public partial class ReceiverB : Form
+    {
+        public ReceiverB()
+        {
+            InitializeComponent();
+        }
+
+        [System.Security.Permissions.PermissionSet(System.Security.Permissions.SecurityAction.Demand, Name = "FullTrust")]
+        protected override void WndProc(ref Message m)
+        {
+
+            if (m.Msg == 0x004A) 
+            {
+    
+                listBox1.Items.Add("和A成功建立连接");
+                MSG msg = Marshal.PtrToStructure<MSG>(m.LParam);   
+                string time = DateTime.Now.ToString("HH:mm:ss");
+
+                listBox1.Items.Add($"{time}: {msg.lpData}");
+            }
+            base.WndProc(ref m);
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
