e6b96e2127ac7b0ca9e988c13c4ff2d550078839
WHUCSstetman <105272933+WHUCSstetman@users.noreply.github.com>
2024-02-17 16:12:30 +0800

    Add files via upload

---
+++ Experiment1/part2/CopyDataStruct/CopyDataStruct/CopyDataStruct.cs
@@ -0,0 +1,29 @@
+﻿using System.Drawing;
+using System.Runtime.InteropServices;
+using System;
+
+namespace CopyDataStruct
+{
+    [StructLayout(LayoutKind.Sequential)]
+    public struct MSG
+    {
+        //public IntPtr hwnd;    // 窗口句柄
+        //public uint message;   // 消息标识符
+        //public string value;   // 消息的内容
+        //public IntPtr wParam;  // 消息的附加信息（WPARAM）
+        //public IntPtr lParam;  // 消息的附加信息（LPARAM）
+        //public DateTime time;  // 消息被放入消息队列的时间
+        //public POINT pt;       // 鼠标位置
+        public IntPtr dwData;
+        public int cbData;
+        [MarshalAs(UnmanagedType.LPStr)]
+        public string lpData;
+    }
+
+    //[StructLayout(LayoutKind.Sequential)]
+    //public struct POINT
+    //{
+    //    public int X;
+    //    public int Y;
+    //}
+}
