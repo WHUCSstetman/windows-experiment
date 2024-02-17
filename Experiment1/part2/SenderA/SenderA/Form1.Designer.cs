4cf08e3de5a4b3bd52c5ecb3a31e60b843e41bd3
WHUCSstetman <105272933+WHUCSstetman@users.noreply.github.com>
2024-02-17 16:15:11 +0800

    Add files via upload

---
+++ Experiment1/part2/SenderA/SenderA/Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace SenderA
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.userInputTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // userInputTextBox
+            // 
+            this.userInputTextBox.BackColor = System.Drawing.SystemColors.Info;
+            this.userInputTextBox.Font = new System.Drawing.Font("宋体", 22F);
+            this.userInputTextBox.Location = new System.Drawing.Point(71, 95);
+            this.userInputTextBox.Margin = new System.Windows.Forms.Padding(2);
+            this.userInputTextBox.Name = "userInputTextBox";
+            this.userInputTextBox.Size = new System.Drawing.Size(839, 58);
+            this.userInputTextBox.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("宋体", 15F);
+            this.label1.Location = new System.Drawing.Point(65, 55);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(208, 30);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "用户输入内容:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(610, 203);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(138, 70);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "直接发送窗口1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(772, 203);
+            this.button2.Margin = new System.Windows.Forms.Padding(2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(138, 70);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "直接发送窗口2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.BackColor = System.Drawing.SystemColors.Info;
+            this.comboBox1.Font = new System.Drawing.Font("宋体", 15F);
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.ItemHeight = 30;
+            this.comboBox1.Items.AddRange(new object[] {
+            "ReceiverB",
+            "MainWindow"});
+            this.comboBox1.Location = new System.Drawing.Point(71, 247);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(430, 38);
+            this.comboBox1.TabIndex = 5;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(433, 324);
+            this.button3.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(138, 70);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "发送消息";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("宋体", 15F);
+            this.label2.Location = new System.Drawing.Point(65, 193);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(178, 30);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "请选择窗口:";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(979, 443);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.userInputTextBox);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox userInputTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Experiment1/part2/SenderA/SenderA/Form1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CopyDataStruct;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+
+namespace SenderA
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        // FindWindow
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+
+        // SendMessage
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, IntPtr lParam);
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            byte[] sarr = System.Text.Encoding.Default.GetBytes(userInputTextBox.Text);
+            int len = sarr.Length;
+            MSG msg;
+            msg.dwData = (IntPtr)Convert.ToInt16(0);//可以是任意值,本例中不用到
+            msg.cbData = len + 1;//指定lpData内存区域的字节数
+            msg.lpData = userInputTextBox.Text;//发送给目标窗口所在进程的数据
+
+            IntPtr hwnd = FindWindow(null, "ReceiverB");
+            if (hwnd != IntPtr.Zero)
+            {
+
+                IntPtr pnt = Marshal.AllocHGlobal(Marshal.SizeOf(msg));
+                Marshal.StructureToPtr(msg, pnt, false);
+                SendMessage(hwnd, 0x004A, 0, pnt);
+            }
+            else
+            {
+                MessageBox.Show("未找到目标窗口");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            byte[] sarr = System.Text.Encoding.Default.GetBytes(userInputTextBox.Text);
+            int len = sarr.Length;
+            MSG msg;
+            msg.dwData = (IntPtr)Convert.ToInt16(0);//可以是任意值,本例中不用到
+            msg.cbData = len + 1;//指定lpData内存区域的字节数
+            msg.lpData = userInputTextBox.Text;//发送给目标窗口所在进程的数据
+
+            IntPtr hwnd = FindWindow(null, "MainWindow");
+            if (hwnd != IntPtr.Zero)
+            {
+                //IntPtr lParam = Marshal.AllocHGlobal(Marshal.SizeOf(msg));
+                ////Struct->IntPtr
+                //Marshal.StructureToPtr(msg, lParam, false);
+
+                IntPtr pnt = Marshal.AllocHGlobal(Marshal.SizeOf(msg));  //初始化消息句柄
+                Marshal.StructureToPtr(msg, pnt, false);                 //使句柄指向消息结构体
+                SendMessage(hwnd, 0x004A, 0, pnt);                       //发送句柄
+            }
+            else
+            {
+                MessageBox.Show("未找到目标窗口");
+            }
+        }
+
+        public string receiver = "";
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            receiver = comboBox1.SelectedItem.ToString();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            byte[] sarr = System.Text.Encoding.Default.GetBytes(userInputTextBox.Text);
+            int len = sarr.Length;
+            MSG msg;
+            msg.dwData = (IntPtr)Convert.ToInt16(0);//可以是任意值,本例中不用到
+            msg.cbData = len + 1;//指定lpData内存区域的字节数
+            msg.lpData = userInputTextBox.Text;//发送给目标窗口所在进程的数据
+            IntPtr hwnd = FindWindow(null, receiver);     //窗体句柄
+            if (hwnd != IntPtr.Zero)
+            {
+
+
+                IntPtr pnt = Marshal.AllocHGlobal(Marshal.SizeOf(msg));  //初始化消息句柄
+                Marshal.StructureToPtr(msg, pnt, false);                 //使句柄指向消息结构体
+                SendMessage(hwnd, 0x004A, 0, pnt);                       //发送句柄
+            }
+            else
+            {
+                MessageBox.Show("未找到目标窗口");
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
