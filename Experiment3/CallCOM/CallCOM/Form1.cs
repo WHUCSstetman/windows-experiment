d9c9998066d1f322b4f6117b932e888b02adc034
WHUCSstetman <105272933+WHUCSstetman@users.noreply.github.com>
2024-02-17 16:34:38 +0800

    Add files via upload

---
+++ Experiment3/CallCOM/CallCOM/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using CreateCOM;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CallCOM
+{
+    public partial class Form1 : Form
+    {
+        IExpress_realize ie;
+        public Form1()
+        {
+            InitializeComponent();
+            Guid guid = new Guid("0520A50E-48DC-41D4-B4BD-5613202C5165");
+            Type type = Type.GetTypeFromCLSID(guid);
+            object iex = Activator.CreateInstance(type);
+            ie = iex as IExpress_realize;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int a = int.Parse(textBox1.Text);
+                int b = int.Parse(textBox2.Text);
+                IExpress_realize ie = new IExpress_realize();
+                label1.Text = "minus: " + ie.minus(a, b); 
+                label2.Text = "divide: " + ie.divide(a, b);
+            }
+            catch(Exception)
+            {
+                label1.Text = "minus: ";
+                label2.Text = "divide: ";
+            }
+        }
+    }
+}
---
+++ Experiment3/CallCOM/CreateCOM/IExpress_realize.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace CreateCOM
+{
+    [Guid("F6373A4E-C31D-49F8-8885-57AB38B2BE37")]
+    [ComVisible(true)]
+    public interface IExpress
+    {
+        [DispId(1)]
+        string minus(int a, int b);
+        [DispId(2)]
+        string divide(int a, int b);//若b为零，则返回“除零错误”；若b不为0，则返回整除表达式，形如“4 = 33 / 8”
+    }
+    [Guid("0520A50E-48DC-41D4-B4BD-5613202C5165")]
+    [ComVisible(true)]
+    [ClassInterface(ClassInterfaceType.None)]
+    [Description("IExpress_realize")]
+    public class IExpress_realize:IExpress
+    {
+        public string minus(int a,int b)
+        {
+            return $"{a - b} = {a} - {b}";
+        }
+        public string divide(int a,int b)
+        {
+            if(b==0)
+            {
+                return "除零错误";
+            }
+            else
+            {
+                return $"{a / b} = {a}/{b}";
+            }
+        }
+    }
+}
